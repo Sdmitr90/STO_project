97845c0c77f3bddc3a71fa3b6f56497881b0c0f3
WorkFrontDramSystem <dramsystem9@mail.ru>
2022-05-12 10:16:47 +0400

    add ContactsView

---
+++ resources/frontend/carservice/src/views/ContactsView2.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="mid">
+        <div class="main container">
+            <Contacts />
+            
+        </div>
+    </section>
+</template>
+
+<script setup>
+import Contacts from "../components/contacts.vue"
+</script>
+
+<style scoped lang="scss">
+
+.mid{
+    background: rgba(0, 0, 0, 0.67);
+}
+    
+
+</style>
