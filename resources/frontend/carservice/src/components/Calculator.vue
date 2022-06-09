1c4f993fd0eac8ae9c8be518ac47318af33db7c3
Ruslan Borodin <BorodinRA@GMail.com>
2022-06-09 13:22:53 +0300

    написал модальное окно записи

--- resources/frontend/carservice/src/components/Calculator.vue
+++ resources/frontend/carservice/src/components/Calculator.vue
@@ -7,44 +7,61 @@
             <form>
                 <div class="wrapper__work" v-for="valueType in typeOfWork" :key="valueType.typeOfWork">
                     <h3 class="wrapper__work__type" @click="valueType.show = !valueType.show, createdNameOfTheWork(valueType.id)">
-                            {{ valueType.title }}
+
+                        {{ valueType.title }}
+
                     </h3>
                     <div class="wrapper__work__name" v-show="valueType.show" v-for="valueName in nameOfTheWork" :key="valueName.nameOfTheWork">
                         <label class="wrapper__work__name__item" v-if="valueType.id === valueName.types">
                             <input type="checkbox" class="wrapper__work__name__item__box" v-model="idCheckedNames" :value="valueName.id">
-                                {{ valueName.title }}
+
+                            {{ valueName.title }}
+
                         </label>
                     </div>
                 </div>
-                <div class="wrapper__btn">
-                    ЗАПИСАТЬСЯ
+                <div class="wrapper__btn-set">
+                    <div class="wrapper__btn" @click="createdCheckedNames">
+                        РАССЧИТАТЬ
+                    </div>
+                    <div class="wrapper__btn" id="show-modal" @click="showModal">
+                        ЗАПИСАТЬСЯ
+                    </div>
                 </div>
+
             </form>
             <div class="wrapper__cost">
                 <p>Примерная стоимость работ</p>
                 <div class="wrapper__cost__coin">
-                    5000 р
+                    {{ totalPrice }} руб.
                 </div>
             </div>
         </div>
+        <RecordForm v-if="isShowModal" @closeModal="closeModal" />
     </section>
 </template>
-
 <script>
 import axios from 'axios'
+import RecordForm from "./RecordForm.vue";
+
 export default {
     name: "Calculator",
+    components: {RecordForm},
     data() {
         return {
             typeOfWork: [],
             nameOfTheWork: [],
             errors: [],
-            idCheckedNames: [] //потом сделать функцию аксиос и передавать массив в нее для отправки
+
+            idCheckedNames: [],
+            totalPrice: 0,
+
+            isShowModal: false,
         }
     },
     created() {
         axios
-            .get('http://localhost:3000/src/api_in/response_1651338166383.json')
+            .get('http://sto/TechServiceType')
             .then(responce => {
                 this.typeOfWork = responce.data
             })
@@ -56,13 +73,35 @@ export default {
         async createdNameOfTheWork(idType) {
             try {
                 const response = await axios
-                    .get('http://localhost:3000/src/api_in/' + idType + '.json')
+
+                    .get('http://sto/TechServiceType/' + idType)
                     .then(responce => {
                         this.nameOfTheWork = responce.data
                     })
             } catch (error) {
                 this.errors.push(error);
             }
+        },
+        async createdCheckedNames() {
+            try {
+                const response = await axios
+                    .post('http://sto/calculate', {
+                        id: this.idCheckedNames
+                    })
+                    .then(responce => {
+                        this.totalPrice = responce.data
+                        console.log(response)
+
+                    })
+            } catch (error) {
+                this.errors.push(error);
+            }
+        },
+        showModal() {
+            this.isShowModal = true;
+        },
+        closeModal() {
+            this.isShowModal = false;
         }
     }
 }
@@ -77,7 +116,7 @@ form {
 
 .wrapper {
     max-width: 1440px;
-    min-height: 723px;
+    min-height: 100%;
     margin: 0 auto;
     padding-top: 88px;
     box-sizing: border-box;
@@ -111,6 +150,9 @@ form {
             font-size: 18px;
         }
     }
+    &__btn-set {
+        display: flex;
+    }
     &__btn {
         width: 296px;
         height: 64px;
@@ -130,7 +172,7 @@ form {
         margin-top: 40px;
         padding-bottom: 200px;
         display: flex;
-        justify-content: space-around;
+        justify-content: space-evenly;
         p {
             font-size: 28px;
             line-height: 38px;
